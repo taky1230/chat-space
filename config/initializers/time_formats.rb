5f6856b2d38ea166bd56fdb620b8487b01868578
taky1230 <taky_1230@yahoo.co.jp>
2020-03-15 13:02:56 +0900

    hidouki fixed2

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,5 @@
-Date::DATE_FORMATS[:default] = "%Y年%m月%d日"
-Date::DATE_FORMATS[:date] = "%m/%d"
-DateTime::DATE_FORMATS[:default] = "%Y年%m月%d日 %H時%M分"
-DateTime::DATE_FORMATS[:datetime] = "%Y/%m/%d %H:%M"
+Time::DATE_FORMATS[:default] = '%Y/%m/%d %H:%M'
+Time::DATE_FORMATS[:datetime] = '%Y/%m/%d %H:%M'
+Time::DATE_FORMATS[:date] = '%Y/%m/%d'
+Time::DATE_FORMATS[:time] = '%H:%M:%S'
+Date::DATE_FORMATS[:default] = '%Y/%m/%d'
